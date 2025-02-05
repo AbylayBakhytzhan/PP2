85096843e5bbe8628d19e15b3d532ccd6933ca4d
AbylayBakhytzhan <bahytzanabylaj6@gmail.com>
2025-02-05 14:41:19 +0600

    Lab_3

---
+++ Lab_3/14.py
@@ -0,0 +1,5 @@
+def grams_to_ounces(grams):
+    return 28.3495231 * grams
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (5 / 9) * (fahrenheit - 32)
---
+++ Lab_3/Functions/9.py
@@ -0,0 +1,2 @@
+def sphere_volume(radius):
+    return (4 / 3) * math.pi * radius**3
